feb2eb2db04dcaecfcf9c968294f5af4fe6cdbd1
Eric <ec6862@gmail.com>
2026-01-07 23:06:25 -0500

    updated OpenAI API Key call

--- agent.py
+++ agent.py
@@ -1,4 +1,9 @@
 from langchain.agents import create_agent
+import getpass, os
+
+if not os.environ.get("OPENAI_API_KEY"):
+    os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter your OpenAI API Key: ")
+
 
 def get_weather(city: str) -> str:
     return f"It's always sunny in {city}"
